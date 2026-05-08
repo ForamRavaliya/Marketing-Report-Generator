fc4e3cfd882940da494ae0ae0d46cd914f416e3e
ForamRavaliya <fravaliya2107@gmail.com>
2026-05-08 20:16:31 +0530

    Update project

--- backend/src/routes/adAccounts.js
+++ backend/src/routes/adAccounts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../db/index.js');
 const { authenticate } = require('../middleware/auth');
 const {
   fetchMetaInsights,
