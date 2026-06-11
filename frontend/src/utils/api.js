1295ca5b2449f65dcae18a3bc0d128234f0189f8
ForamRavaliya <fravaliya2107@gmail.com>
2026-06-11 19:23:08 +0530

    Update project

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -93,8 +93,10 @@ export const confirmUploadMapping = (uploadId, mapping) =>
   api.post(`/uploads/${uploadId}/confirm-mapping`, { mapping })
     .then(res => res.data);
  // Subscription
-    export const getSubscription = () =>
-      api.get('/subscription').then(r => r.data);
+    export const getSubscription = async () => {
+      const res = await api.get('/subscription/current');
+      return res.data;
+    };
 
     export const updateSubscriptionPlan = (planName) =>
       api.put('/subscription/plan', { planName }).then(r => r.data);
